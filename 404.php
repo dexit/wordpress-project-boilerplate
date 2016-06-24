333d3731d0bf7282f02c614fd80291009e38d32f
Ollie <ojreardon@gmail.com>
2016-06-24 14:09:07 +0100

    "return"

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="container">
 	<h2 class="text-center">Page not found...</h2>
-	<p>Unfortunately, the page you are looking for can not be found. Please check that you have entered in the correct address, you could also <a href="/">Return to the homepage</a> and try again.</p>
+	<p>Unfortunately, the page you are looking for can not be found. Please check that you have entered in the correct address, you could also <a href="/">return to the homepage</a> and try again.</p>
 </div>
 <?php get_footer(); ?>
